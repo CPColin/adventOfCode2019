c6fc2b7c35867c680c4fcc6fc46cb11964c47b3c
Colin Bartolome <cbartolo@crappycomic.com>
2020-01-03 17:43:59 -0800

    Day 12 part 1 and part 2 that doesn't work yet

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -1,5 +1,9 @@
 package day12
 
+import day12.Day12.Moon
+import day12.Day12.Position
+import day12.Day12.System
+import kotlin.math.abs
 import kotlin.math.sign
 
 class Day12 {
@@ -11,11 +15,54 @@ class Day12 {
         operator fun plus(other: Velocity) = Velocity(x + other.x, y + other.y, z + other.z)
     }
 
-    data class Moon(val label: String, val position: Position, val velocity: Velocity = Velocity(0, 0, 0))
+    data class Moon(val position: Position, val velocity: Velocity = Velocity(0, 0, 0)) {
+        fun applyGravity(otherMoons: List<Moon>): Moon {
+            val deltaX = otherMoons.map { gravitySign(position.x, it.position.x) }.sum()
+            val deltaY = otherMoons.map { gravitySign(position.y, it.position.y) }.sum()
+            val deltaZ = otherMoons.map { gravitySign(position.z, it.position.z) }.sum()
 
-    data class System(val io: Moon, val europa: Moon, val callisto: Moon, val ganymede: Moon)
+            return copy(velocity = Velocity(velocity.x + deltaX, velocity.y + deltaY, velocity.z + deltaZ))
+        }
 
-    companion object {
-        fun gravitySign(position1: Int, position2: Int) = (position2 - position1).sign
+        fun applyVelocity() = copy(position = position + velocity)
+
+        val kineticEnergy = abs(velocity.x) + abs(velocity.y) + abs(velocity.z)
+
+        val potentialEnergy = abs(position.x) + abs(position.y) + abs(position.z)
+
+        val totalEnergy = potentialEnergy * kineticEnergy
+
+        companion object {
+            fun gravitySign(position1: Int, position2: Int) = (position2 - position1).sign
+        }
     }
+
+    data class System(val moons: List<Moon>) {
+        fun findRepeat() = ticks.indexOfFirst { this == it } + 1
+
+        fun tick() = copy(
+            moons = moons.map { it.applyGravity(moons - it) }.map { it.applyVelocity() }
+        )
+
+        fun ticks(count: Int) = ticks.take(count).last()
+
+        private val ticks = generateSequence(this, System::tick).drop(1)
+
+        val totalEnergy = moons.map(Moon::totalEnergy).sum()
+    }
+}
+
+fun main() {
+    val system = System(
+        listOf(
+            Moon(Position(4, 12, 13)),
+            Moon(Position(-9, 14, -3)),
+            Moon(Position(-7, -1, 2)),
+            Moon(Position(-11, 17, -1))
+        )
+    )
+
+    println("Energy after 1000 ticks: ${system.ticks(1000).totalEnergy}")
+
+    println("Repeats after ${system.findRepeat()} ticks")
 }
---
+++ src/test/kotlin/day12/Day12Tests.kt
@@ -0,0 +1,255 @@
+package day12
+
+import day12.Day12.Moon
+import day12.Day12.Position
+import day12.Day12.System
+import day12.Day12.Velocity
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
+import util.ParameterizedTestClass
+import util.TestParameters
+import java.util.stream.Stream
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@ParameterizedTestClass
+class Day12Tests {
+    @TestParameters
+    private fun moonApplyGravityParameters() = Stream.of(
+        Arguments.of(
+            Moon(Position(1, 2, 3), Velocity(5, 5, 5)),
+            listOf(Moon(Position(3, 2, 1))),
+            Moon(Position(1, 2, 3), Velocity(6, 5, 4))
+        ),
+        Arguments.of(
+            Moon(Position(1, 2, 3), Velocity(5, 5, 5)),
+            listOf(Moon(Position(3, 2, 1)), Moon(Position(5, 5, 0))),
+            Moon(Position(1, 2, 3), Velocity(7, 6, 3))
+        )
+    )
+
+    @ParameterizedTest
+    @MethodSource("moonApplyGravityParameters")
+    fun moonApplyGravity(moon: Moon, otherMoons: List<Moon>, expected: Moon) {
+        assertEquals(expected, moon.applyGravity(otherMoons))
+    }
+
+    @Test
+    fun moonApplyVelocity() {
+        val initialMoon = Moon(Position(1, 2, 3), Velocity(-2, 0, 3))
+        val expectedMoon = Moon(Position(-1, 2, 6), Velocity(-2, 0, 3))
+
+        assertEquals(expectedMoon, initialMoon.applyVelocity())
+    }
+
+    @TestParameters
+    private fun moonGravitySignParameters() = Stream.of(
+        Arguments.of(3, 5, +1),
+        Arguments.of(5, 3, -1),
+        Arguments.of(3, 3, 0)
+    )
+
+    @ParameterizedTest
+    @MethodSource("moonGravitySignParameters")
+    fun moonGravitySign(position1: Int, position2: Int, expected: Int) {
+        assertEquals(expected, Moon.gravitySign(position1, position2))
+    }
+
+    @TestParameters
+    private fun moonKineticEnergyParameters() = Stream.of(
+        Arguments.of(Moon(Position(2, 1, -3), Velocity(-3, -2, 1)), 6),
+        Arguments.of(Moon(Position(1, -8, 0), Velocity(-1, 1, 3)), 5),
+        Arguments.of(Moon(Position(3, -6, 1), Velocity(3, 2, -3)), 8),
+        Arguments.of(Moon(Position(2, 0, 4), Velocity(1, -1, -1)), 3)
+    )
+
+    @ParameterizedTest
+    @MethodSource("moonKineticEnergyParameters")
+    fun moonKineticEnergy(moon: Moon, expected: Int) {
+        assertEquals(expected, moon.kineticEnergy)
+    }
+
+    @TestParameters
+    private fun moonPotentialEnergyParameters() = Stream.of(
+        Arguments.of(Moon(Position(2, 1, -3), Velocity(-3, -2, 1)), 6),
+        Arguments.of(Moon(Position(1, -8, 0), Velocity(-1, 1, 3)), 9),
+        Arguments.of(Moon(Position(3, -6, 1), Velocity(3, 2, -3)), 10),
+        Arguments.of(Moon(Position(2, 0, 4), Velocity(1, -1, -1)), 6)
+    )
+
+    @ParameterizedTest
+    @MethodSource("moonPotentialEnergyParameters")
+    fun moonPotentialEnergy(moon: Moon, expected: Int) {
+        assertEquals(expected, moon.potentialEnergy)
+    }
+
+    @TestParameters
+    private fun moonTotalEnergyParameters() = Stream.of(
+        Arguments.of(Moon(Position(2, 1, -3), Velocity(-3, -2, 1)), 36),
+        Arguments.of(Moon(Position(1, -8, 0), Velocity(-1, 1, 3)), 45),
+        Arguments.of(Moon(Position(3, -6, 1), Velocity(3, 2, -3)), 80),
+        Arguments.of(Moon(Position(2, 0, 4), Velocity(1, -1, -1)), 18)
+    )
+
+    @ParameterizedTest
+    @MethodSource("moonTotalEnergyParameters")
+    fun moonTotalEnergy(moon: Moon, expected: Int) {
+        assertEquals(expected, moon.totalEnergy)
+    }
+
+    @Test
+    fun positionPlusVelocity() {
+        assertEquals(Moon(Position(-1, 2, 6)), Moon(Position(1, 2, 3) + Velocity(-2, 0, 3)))
+    }
+
+    @TestParameters
+    private fun systemFindRepeatParameters() = Stream.of(
+        Arguments.of(
+            listOf(
+                Moon(Position(-1, 0, 2)),
+                Moon(Position(2, -10, -7)),
+                Moon(Position(4, -8, 8)),
+                Moon(Position(3, 5, -1))
+            ),
+            2772
+        )
+    )
+
+    @ParameterizedTest
+    @MethodSource("systemFindRepeatParameters")
+    fun systemFindRepeat(moons: List<Moon>, expected: Int) {
+        val system = System(moons)
+
+        assertEquals(expected, system.findRepeat())
+    }
+
+    private val systemTicks = listOf(
+        System(
+            listOf(
+                Moon(Position(-1, 0, 2), Velocity(0, 0, 0)),
+                Moon(Position(2, -10, -7), Velocity(0, 0, 0)),
+                Moon(Position(4, -8, 8), Velocity(0, 0, 0)),
+                Moon(Position(3, 5, -1), Velocity(0, 0, 0))
+            )
+        ),
+        System(
+            listOf(
+                Moon(Position(2, -1, 1), Velocity(3, -1, -1)),
+                Moon(Position(3, -7, -4), Velocity(1, 3, 3)),
+                Moon(Position(1, -7, 5), Velocity(-3, 1, -3)),
+                Moon(Position(2, 2, 0), Velocity(-1, -3, 1))
+            )
+        ),
+        System(
+            listOf(
+                Moon(Position(5, -3, -1), Velocity(3, -2, -2)),
+                Moon(Position(1, -2, 2), Velocity(-2, 5, 6)),
+                Moon(Position(1, -4, -1), Velocity(0, 3, -6)),
+                Moon(Position(1, -4, 2), Velocity(-1, -6, 2))
+            )
+        ),
+        System(
+            listOf(
+                Moon(Position(5, -6, -1), Velocity(0, -3, 0)),
+                Moon(Position(0, 0, 6), Velocity(-1, 2, 4)),
+                Moon(Position(2, 1, -5), Velocity(1, 5, -4)),
+                Moon(Position(1, -8, 2), Velocity(0, -4, 0))
+            )
+        ),
+        System(
+            listOf(
+                Moon(Position(2, -8, 0), Velocity(-3, -2, 1)),
+                Moon(Position(2, 1, 7), Velocity(2, 1, 1)),
+                Moon(Position(2, 3, -6), Velocity(0, 2, -1)),
+                Moon(Position(2, -9, 1), Velocity(1, -1, -1))
+            )
+        ),
+        System(
+            listOf(
+                Moon(Position(-1, -9, 2), Velocity(-3, -1, 2)),
+                Moon(Position(4, 1, 5), Velocity(2, 0, -2)),
+                Moon(Position(2, 2, -4), Velocity(0, -1, 2)),
+                Moon(Position(3, -7, -1), Velocity(1, 2, -2))
+            )
+        ),
+        System(
+            listOf(
+                Moon(Position(-1, -7, 3), Velocity(0, 2, 1)),
+                Moon(Position(3, 0, 0), Velocity(-1, -1, -5)),
+                Moon(Position(3, -2, 1), Velocity(1, -4, 5)),
+                Moon(Position(3, -4, -2), Velocity(0, 3, -1))
+            )
+        ),
+        System(
+            listOf(
+                Moon(Position(2, -2, 1), Velocity(3, 5, -2)),
+                Moon(Position(1, -4, -4), Velocity(-2, -4, -4)),
+                Moon(Position(3, -7, 5), Velocity(0, -5, 4)),
+                Moon(Position(2, 0, 0), Velocity(-1, 4, 2))
+            )
+        ),
+        System(
+            listOf(
+                Moon(Position(5, 2, -2), Velocity(3, 4, -3)),
+                Moon(Position(2, -7, -5), Velocity(1, -3, -1)),
+                Moon(Position(0, -9, 6), Velocity(-3, -2, 1)),
+                Moon(Position(1, 1, 3), Velocity(-1, 1, 3))
+            )
+        ),
+        System(
+            listOf(
+                Moon(Position(5, 3, -4), Velocity(0, 1, -2)),
+                Moon(Position(2, -9, -3), Velocity(0, -2, 2)),
+                Moon(Position(0, -8, 4), Velocity(0, 1, -2)),
+                Moon(Position(1, 1, 5), Velocity(0, 0, 2))
+            )
+        ),
+        System(
+            listOf(
+                Moon(Position(2, 1, -3), Velocity(-3, -2, 1)),
+                Moon(Position(1, -8, 0), Velocity(-1, 1, 3)),
+                Moon(Position(3, -6, 1), Velocity(3, 2, -3)),
+                Moon(Position(2, 0, 4), Velocity(1, -1, -1))
+            )
+        )
+    )
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    fun systemTick(index: Int) {
+        val initialSystem = systemTicks[index - 1]
+        val expectedSystem = systemTicks[index]
+
+        assertEquals(expectedSystem, initialSystem.tick())
+    }
+
+    @Test
+    fun systemTotalEnergy() {
+        val system = System(
+            listOf(
+                Moon(Position(2, 1, -3), Velocity(-3, -2, 1)),
+                Moon(Position(1, -8, 0), Velocity(-1, 1, 3)),
+                Moon(Position(3, -6, 1), Velocity(3, 2, -3)),
+                Moon(Position(2, 0, 4), Velocity(1, -1, -1))
+            )
+        )
+
+        assertEquals(179, system.totalEnergy)
+    }
+
+    @Test
+    fun systemTotalEnergy2() {
+        val system = System(
+            listOf(
+                Moon(Position(-8, -10, 0)),
+                Moon(Position(5, 5, 10)),
+                Moon(Position(2, -7, 3)),
+                Moon(Position(9, -8, -3))
+            )
+        )
+
+        assertEquals(1940, system.ticks(100).totalEnergy)
+    }
+}
