cd1da46e5dc071f636c3046105ff63e17ce2f2a5
Colin Bartolome <cbartolo@crappycomic.com>
2020-01-04 21:23:48 -0800

    Day 13 part 2, lazily

--- src/main/kotlin/day13/Day13.kt
+++ src/main/kotlin/day13/Day13.kt
@@ -1,8 +1,9 @@
 package day13
 
 import day11.Intcode
+import kotlin.math.sign
 
-data class Point(val x: Int, val y: Int)
+data class Point(val x: Long, val y: Long)
 
 enum class Tile {
     EMPTY,
@@ -12,19 +13,80 @@ enum class Tile {
     BALL
 }
 
-fun main() {
-    val memory = ::main.javaClass.getResourceAsStream("input.txt").bufferedReader().readLine()
+fun countBlocks(memory: String) {
     val intcode = Intcode(
         inputs = emptyList(),
         memory = memory
     )
+
     val outputs = generateSequence {
         intcode.runUntilOutput()
         intcode.output
     }
-    val tiles = outputs.chunked(3) { (x, y, tile) ->
-        Point(x.toInt(), y.toInt()) to Tile.values()[tile.toInt()]
+    val tiles = outputs.chunked(3) { (x, y, value) ->
+        Point(x, y) to value.toInt()
     }.toMap()
 
-    println(tiles.count { it.value == Tile.BLOCK })
+    println("Blocks: ${tiles.count { it.value == Tile.BLOCK.ordinal }}")
+}
+
+fun playGame(memory: String) {
+    val intcode = Intcode(
+        inputs = emptyList(),
+        memory = memory
+    )
+
+    intcode.memory[0] = 2 // Insert coin
+
+    var ball: Point? = null
+    var paddle: Point? = null
+    var score = 0L
+    val output = mutableListOf<Long>()
+    val tiles = mutableMapOf<Point, Tile>()
+
+    while (true) {
+        val joystick = if (ball == null || paddle == null) 0 else (ball.x - paddle.x).sign
+        
+        intcode.inputs.clear()
+        intcode.inputs.add(joystick.toLong())
+        
+        intcode.runUntilOutput()
+        
+        if (intcode.output == null) {
+            break
+        } else {
+            output.add(intcode.output!!)
+        }
+        
+        if (output.size == 3) {
+            val (x, y, value) = output
+            
+            output.clear()
+            
+            if (x == -1L && y == 0L) {
+                score = value
+            } else {
+                val point = Point(x, y)
+                val tile = Tile.values()[value.toInt()]
+                
+                if (tile == Tile.BALL) {
+                    ball = point
+                } else if (tile == Tile.PADDLE) {
+                    paddle = point
+                }
+                
+                tiles[point] = tile
+            }
+        }
+    }
+
+    println("Final score: $score")
+}
+
+fun main() {
+    val memory = ::main.javaClass.getResourceAsStream("input.txt").bufferedReader().readLine()
+
+    countBlocks(memory)
+    
+    playGame(memory)
 }
